af4119451f4ab6db02fff4515efe92728cdedb33
sandjar <b-sandj@hotmail.com>
2015-07-15 17:30:56 +0200

    initial BlackJack

---
+++ projektBlackJack.gr1.berdievs14.bergerru14/src/at/fhj/swd2/project/berdievs14bergerru14/BlackJack.java
@@ -0,0 +1,10 @@
+package at.fhj.swd2.project.berdievs14bergerru14;
+
+public class BlackJack {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ projektBlackJack.gr1.berdievs14.bergerru14/test/at/fhj/swd2/project/berdievs14bergerru14/TestBlackJack.java
@@ -0,0 +1,14 @@
+package at.fhj.swd2.project.berdievs14bergerru14;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestBlackJack {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
